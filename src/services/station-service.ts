e5a7a473adc966cbffe125a31b2e43609a4c3560
mitchellday6 <mitchell.day.6@gmail.com>
2025-04-07 09:09:14 +1000

    Fix commit wrong main branch push

--- src/services/station-service.ts
+++ src/services/station-service.ts
@@ -1,13 +1,10 @@
 import ChargingStationRepository from "../repositories/station-repository";
 
 export default class ChargingStationService {
-<<<<<<< HEAD
-=======
  async getAllStations() {
   return await ChargingStationRepository.findAll();
  }
 
->>>>>>> 2ef7dcf44cf11a3a99ca9907205f20bee56614bb
   async getStationById(stationId: string) {
     return await ChargingStationRepository.findById(stationId);
   }
@@ -15,11 +12,8 @@ export default class ChargingStationService {
   async getStationsWithIdIn(stationIds: string[]) {
     return await ChargingStationRepository.findByIdIn(stationIds);
   }
-<<<<<<< HEAD
-=======
 
   async getNearestStation(lat: number, lon: number) {
     return await ChargingStationRepository.findNearest(lat, lon)
   }
->>>>>>> 2ef7dcf44cf11a3a99ca9907205f20bee56614bb
 }
